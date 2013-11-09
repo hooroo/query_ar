97af574f590717eac99e2edd99c5fd826f00909c
Stuart Liston <stuart.liston@gmail.com>
2013-11-10 00:33:21 +1100

    Adding some clarification to AsctiveRecord::Bass

--- spec/support/active_record/bass.rb
+++ spec/support/active_record/bass.rb
@@ -1,5 +1,23 @@
 require 'active_support/core_ext'
 
+# To avoind having ActiveRecord as a development dependency of
+# this gem (whose sole purpose is to chain AR queries), we use
+# this class in tests. Usage is as follows.
+
+# Declare a class that inherits us instead of AR::Base
+#
+#   class User < ActiveRecord::Bass
+#   end
+
+# Chain a bunch of query methods together:
+#
+#   > User.active.where(name: 'stu').order('name').limit(10).offset(0)
+
+# Query what messages where received and with what args, for assertions
+#
+#   > User.messages_received
+#   => {:active=>[], :where=>[{:name=>"stu"}], :order=>["name"], :limit=>[10], :offset=>[0]}
+
 module ActiveRecord
   class Bass
 
