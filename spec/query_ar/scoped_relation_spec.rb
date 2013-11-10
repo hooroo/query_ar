d0ddaaf7fcb3afd3d7d2d0556601a2d50e2afe5e
Stuart Liston <stuart.liston@gmail.com>
2013-11-10 14:05:32 +1100

    Adding slightly better specs for ScopedModel

--- spec/query_ar/scoped_relation_spec.rb
+++ spec/query_ar/scoped_relation_spec.rb
@@ -1,10 +1,22 @@
+require 'spec_helper'
 require 'query_ar/scoped_relation'
+
+# A class upon which we can base our query object.
+# See ActiveRecord::Face for explanation on usage.
+#
+class User < ActiveRecord::Face
+end
 
 describe ScopedRelation do
 
   describe "#scoped" do
 
-
+    it "returns the relation with all scopes applied" do
+      scopes = {older_than: 30, younger_than: 50}
+      ScopedRelation.new(User, scopes).scoped
+      expect(User.messages_received).to include(older_than: [30])
+      expect(User.messages_received).to include(younger_than: [50])
+    end
   end
 
 end
